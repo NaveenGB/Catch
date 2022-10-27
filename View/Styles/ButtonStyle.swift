efa32b42558082d3655ce2363b83ca1d4ec37ff7
NaveenGB <naveen.ross@gmail.com>
2022-10-27 11:01:25 +0530

    Create ButtonStyle.swift

---
+++ View/Styles/ButtonStyle.swift
@@ -0,0 +1,37 @@
+//
+//  ButtonStyle.swift
+//  Calculater
+//
+//  Created by G Bannikoppa, Naveen on 26/10/22.
+//
+
+import SwiftUI
+
+struct CalculatorButtonStyle: ButtonStyle {
+    
+    var size: CGFloat
+    var backgroundColor: Color
+    var foregroundColor: Color
+    var isWide: Bool = false
+    
+    func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .font(.system(size: 32, weight: .medium))
+                .frame(width: size, height: size)
+                .frame(maxWidth: isWide ? .infinity : size, alignment: .leading)
+                .background(backgroundColor)
+                .foregroundColor(foregroundColor)
+                .overlay {
+                    if configuration.isPressed {
+                        Color(white: 1.0, opacity: 0.2)
+                    }
+                }
+                .clipShape(Capsule())
+    }
+}
+
+//struct CalculatorButtonStyle_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CalculatorButtonStyle()
+//    }
+//}
